1ff680d6ce7ec5f9f1a0eb909fd113616c6f60f7
Vincent <vshan0816@gmail.com>
2021-09-28 16:51:50 -0400

    commit tree dbd80bf9d9181247fe93464d03349b4603d4f0d4

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function  writeCards(array, event){
+    const newArray = []
+    for (let i = 0; i< array.length; i++){
+        newArray.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`)
+    }
+    return newArray
+}
+
+function countDown(integer) {
+    let i = integer;
+    while (i>-1){
+        console.log(i)
+        i--
+    }
+}
